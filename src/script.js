2d8211d2c2f633239348bb64d58617fa52a86005
Loic-C <49689982+Loic-C@users.noreply.github.com>
2020-05-14 15:09:19 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function() {
-  http.get('http://test.k6.io');
+  http.get('http://example-prometheus-nodejs-thomas-test-cd-qlf.qlf.co.as8677.net/');
   sleep(1);
 }
